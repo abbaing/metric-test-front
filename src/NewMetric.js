2d0702bd5bd694f873a12da43d4065fa6b56aefd
David Abba <abba.sistemas@gmail.com>
2021-12-09 03:21:00 -0300

    Added metric form

--- src/NewMetric.js
+++ src/NewMetric.js
@@ -1,24 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Button, Modal } from 'react-bootstrap';
-import React, { useState } from "react";
+import React from "react";
+import Form from 'react-bootstrap/Form'
 
 function NewMetric(props) {
     return (
       <div>
-        <Modal show={props.isVisible} onHide={()=> props.onClickCloseModal()}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={()=> props.onClickCloseModal()}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={()=> props.onClickCloseModal()}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <Form>
+          <Modal show={props.isVisible} onHide={()=> props.onClickCloseModal()}>
+            <Modal.Header closeButton>
+              <Modal.Title>New Metric</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Please, complete the following form:
+                <Form.Group className="mb-3">
+                  <Form.Label>Metric name</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Metric value</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="default" onClick={()=> props.onClickCloseModal()}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={()=> props.onClickCloseModal()} type="submit">
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Form>
       </div>
     );
   }
