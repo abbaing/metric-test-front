c339c0e421ccff1ab4d8d7ced3c8d58a6a96abf8
Ing. Abba David <david_abba@outlook.com>
2021-12-09 17:43:49 -0300

    Metrics from backend working

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -7,97 +7,50 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import './Timeline.css';
 
 export default class Timeline extends React.Component {
-    render() {
-        return (
-        <VerticalTimeline>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2011 - present"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-              icon={<FontAwesomeIcon icon={faCheckCircle} className="inside-button" />}
-            >
-              <h3 className="vertical-timeline-element-title">Creative Director</h3>
-              <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-              <p>
-                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2010 - 2011"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-              icon={<FontAwesomeIcon icon={faCheckCircle} className="inside-button" />}
-            >
-              <h3 className="vertical-timeline-element-title">Art Director</h3>
-              <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-              <p>
-                Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2008 - 2010"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-              icon={<FontAwesomeIcon icon={faCheckCircle} className="inside-button" />}
-            >
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-              <p>
-                User Experience, Visual Design
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work"
-              date="2006 - 2008"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-              icon={<FontAwesomeIcon icon={faCheckCircle} className="inside-button" />}
-            >
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-              <p>
-                User Experience, Visual Design
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="April 2013"
-              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-              //icon={<SchoolIcon />}
-            >
-              <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-              <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-              <p>
-                Strategy, Social Media
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="November 2012"
-              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-              //icon={<SchoolIcon />}
-            >
-              <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-              <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-              <p>
-                Creative Direction, User Experience, Visual Design
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="2002 - 2006"
-              iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-              //icon={<SchoolIcon />}
-            >
-              <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-              <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-              <p>
-                Creative Direction, Visual Design
-              </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-              //icon={<StarIcon />}
-            />
-        </VerticalTimeline>);
+
+  constructor() {
+    super();
+    this.state = {
+      metrics: []
     }
+  }
+
+  componentDidMount(){
+    fetch(process.env.REACT_APP_API_URL + '/metrics')
+    .then((response) => response.json())
+    .then(metrics => this.setState({metrics}))
+  }
+
+  formatDate(dateString) {
+    const options = { 
+      year: "numeric", 
+      month: "long", 
+      day: "numeric",
+      hour: "numeric",
+      minute: "2-digit", 
+    }
+    return new Date(dateString).toLocaleDateString(undefined, options)
+  }
+
+  render() {
+      return (
+      <VerticalTimeline>
+        <ul>
+          {this.state.metrics.map(metric => (
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={this.formatDate(metric.time)}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon={<FontAwesomeIcon icon={faCheckCircle} className="inside-button" />}
+              >
+                <h3 className="vertical-timeline-element-title">Metric registry</h3>
+                <h4 className="vertical-timeline-element-subtitle">{metric.name}</h4>
+                <p>
+                  {metric.value}
+                </p>
+              </VerticalTimelineElement>
+          ))}
+        </ul>
+      </VerticalTimeline>);
+  }
 }
