6961998d3d70f08f0db4f43b9988862bd6332170
Ing. Abba David <david_abba@outlook.com>
2021-12-14 17:54:09 -0300

    Added test for title validation

--- src/components/DeleteMetric.test.js
+++ src/components/DeleteMetric.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Modal } from 'react-bootstrap';
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
 import DeleteMetric from './DeleteMetric';
 
 describe('Testing the component <DeleteMetric />', () => {    
     let component = shallow(<DeleteMetric />);
-	test('It should shows <DeleteMetric /> correctly', () => {
+	
+    test('It should shows <DeleteMetric /> correctly', () => {
         expect(component).toMatchSnapshot();
+        component.unmount();
+	});
+
+    test('It should shows <DeleteMetric /> with visible flag', () => {
+        const isDeleteMetricVisible = true;
+        component = shallow(<DeleteMetric isVisible={isDeleteMetricVisible} />);
+        expect(component.find(Modal.Title).text()).toContain('Confirm');
         component.unmount();
 	});
 });
